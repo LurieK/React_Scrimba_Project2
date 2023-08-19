d0df9a7528f1c03783acc9b091f602d9d1033eaa
Lurie <lurainekimmerle@gmail.com>
2023-08-19 03:23:34 +0000

    add data object with location information and import images

---
+++ src/data/data.js
@@ -0,0 +1,145 @@
+import aguadilla from "./data-img/aguadilla.png";
+import fuerteventura from "./data-img/fuerteventura.png";
+import bali from "./data-img/bali.png";
+import bangkok from "./data-img/Bangkok.png";
+import benevento from "./data-img/Benevento.png";
+import cabo from "./data-img/cabo.png";
+import cebu from "./data-img/cebu.png";
+import ho_chi_minh from "./data-img/ho_chi_minh.png";
+import kl from "./data-img/KL.png";
+import perth from "./data-img/perth.png";
+import singapore from "./data-img/singapore.png";
+import surfers_bay from "./data-img/surfers-bay.png";
+import taipei from "./data-img/Taipei.png";
+
+const data = [
+    {
+      key: 1,
+      Country: "Barbados",
+      Location: "Surfers Bay",
+      GoogleMapLink: "https://dummy.link",
+      Dates: "November 2020-February 2021",
+      about: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      image: {surfers_bay}
+    },
+    {
+      key: 2,
+      Country: "Puerto Rico",
+      Location: "Aguadilla",
+      GoogleMapLink: "https://dummy.link",
+      Dates: "February 2021-March 2021",
+      about: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      image: {aguadilla}
+    },
+    {
+      key: 3,
+      Country: "Mexico",
+      Location: "San Jose del Cabo",
+      GoogleMapLink: "https://dummy.link",
+      Dates: "April 2021-August 2021",
+      about: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      image: {cabo}
+    },
+    {
+      key: 4,
+      Country: "Italy",
+      Location: "Ostia & Benevento",
+      GoogleMapLink: "https://dummy.link",
+      Dates: "September 2021-October 2021",
+      about: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      image: {benevento} 
+    },
+    {
+      key: 5,
+      Country: "Spain",
+      Location: "Fuerteventura",
+      GoogleMapLink: "https://dummy.link",
+      Dates: "October 2021-December 2021",
+      about: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      image: {fuerteventura}
+    },
+    {
+      key: 6,
+      Country: "Indonesia",
+      Location: "Bali",
+      GoogleMapLink: "https://dummy.link",
+      Dates: "May 2022",
+      about: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      image: {bali}
+    },
+    {
+      key: 7,
+      Country: "Singapore",
+      Location: "Singapore",
+      GoogleMapLink: "https://dummy.link",
+      Dates: "July 2022",
+      about: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      image: {singapore}
+    },
+    {
+      key: 8,
+      Country: "Malaysia",
+      Location: "Kuala Lumpur",
+      GoogleMapLink: "https://dummy.link",
+      Dates: "July 2022",
+      about: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      image: {kl}
+    },
+    {
+      key: 9,
+      Country: "Australia",
+      Location: "Perth",
+      GoogleMapLink: "https://dummy.link",
+      Dates: "September 2023",
+      about: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      image: {perth}
+    },
+    {
+      key: 10,
+      Country: "Thailand",
+      Location: "Bangkok",
+      GoogleMapLink: "https://dummy.link",
+      Dates: "November 2022-December 2022",
+      about: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      image: {bangkok}
+    },
+    {
+      key: 11,
+      Country: "Taiwan",
+      Location: "Taipei",
+      GoogleMapLink: "https://dummy.link",
+      Dates: "February 2023",
+      about: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      image: {taipei}
+    },
+    {
+      key: 12,
+      Country: "Vietnam",
+      Location: "Ho Chi Minh",
+      GoogleMapLink: "https://dummy.link",
+      Dates: "April 2023",
+      about: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      image: {ho_chi_minh}
+    },
+    {
+      key: 13,
+      Country: "Philippines",
+      Location: "Cebu",
+      GoogleMapLink: "https://dummy.link",
+      Dates: "June 2023",
+      about: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      image: {cebu}
+    },
+    {
+      key: 14,
+      Country: "Japan",
+      Location: "Tokyo",
+      GoogleMapLink: "https://dummy.link",
+      Dates: "September 2023",
+      about: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      // Add the corresponding image if available
+    }
+  ];
+
+  export default data
+  
