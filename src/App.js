84e100908826708e00ceb0cafe84f355f408702f
Lurie <lurainekimmerle@gmail.com>
2023-08-19 03:25:45 +0000

    import data.js to app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./styles.css";
 import Card from "./Card.js"
 import Header from "./Header"
+import data from "./data/data.js"
 
 export default function App() {
   return (
     <div className="App">
       <Header/>
-      <Card/>
+      <Card
+      />
     </div>
   );
 }
